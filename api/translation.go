c6303c737c241f0185fae4ad69a9f846a77ae54b
Jeong Hyunseok <nicewook@hotmail.com>
2019-07-15 19:54:49 +0900

    Implementation Phase 2. Local Server which receive Slack's HTTP Post and Translate it back

--- api/translation.go
+++ api/translation.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"bytes"
+	"context"
 	"crypto/hmac"
 	"crypto/sha256"
 	"encoding/hex"
@@ -11,8 +12,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
+	"time"
 
+	"cloud.google.com/go/translate"
 	"github.com/nlopes/slack"
+	"golang.org/x/text/language"
 )
 
 const (
@@ -20,7 +25,23 @@ const (
 	hTimestamp = "X-Slack-Request-Timestamp"
 )
 
+// Hangul related constants.
+const (
+	HangulBase = 0xAC00
+	HangulEnd  = 0xD7A4
+)
+
 var slackSigningSecret string
+
+func isHangul(s string) bool {
+
+	for _, char := range s {
+		if HangulBase <= char && char < HangulEnd {
+			return true
+		}
+	}
+	return false
+}
 
 func init() {
 	// set SLACK_SIGNING_SECRET=<Signing Secret of your Slack App> in windows cli
@@ -30,7 +51,7 @@ func init() {
 // TranslateEnglish2Korean is Translation function
 func TranslateEnglish2Korean(w http.ResponseWriter, r *http.Request) {
 
-	// Verify Slack Request with Signing Secret
+	// Verify Slack Request with Signing Secret, and Timeout check
 	if ok := verifySlackSignature(r, []byte(slackSigningSecret)); ok == false {
 		w.WriteHeader(http.StatusInternalServerError)
 		log.Println("failed on VerifyRequest()")
@@ -45,17 +66,45 @@ func TranslateEnglish2Korean(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Print what Slack sent
-	b, err := json.MarshalIndent(s, "", "--")
+	// Creates a client.
+	ctx := context.Background()
+	client, err := translate.NewClient(ctx)
 	if err != nil {
-		log.Printf("failed to MarchalIndent: %v", err)
+		log.Fatalf("Failed to create client: %v", err)
 	}
-	slackPost := fmt.Sprintf("HTTP POST from Slack\n%s\n", string(b))
-	fmt.Println(slackPost)
+
+	// check if it's Korean
+	var targetLanguage string
+	if isHangul(s.Text) {
+		targetLanguage = "en"
+	} else {
+		targetLanguage = "ko"
+	}
+	// Sets the target language.
+	target, err := language.Parse(targetLanguage)
+	if err != nil {
+		log.Fatalf("failed to parse target language: %v", err)
+	}
+
+	// Translates the text into en <-> ko
+	translations, err := client.Translate(ctx, []string{s.Text}, target, nil)
+	if err != nil {
+		log.Fatalf("failed to translate text: %v", err)
+	}
+
+	srcText := s.Text
+	tgtText := translations[0].Text
+	fmt.Printf("source: %v\n", srcText)
+	fmt.Printf("target: %v\n", tgtText)
 
 	// Send back to Slack
-	params := &slack.Msg{Text: slackPost}
-	b, err = json.Marshal(params)
+	slactPost := fmt.Sprintf("`source`: %s\n`target`: %s\n", srcText, tgtText)
+	params := &slack.Msg{
+		Type: "mrkdwn",
+		Text: slactPost,
+	}
+
+	b, err := json.Marshal(params)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
@@ -91,5 +140,25 @@ func verifySlackSignature(r *http.Request, slackSigningSecret []byte) bool {
 	slackSignature := r.Header.Get(hSignature)
 	message := "v0:" + timestamp + ":" + string(bodyBytes)
 
+	// Timeout check
+	ts, err := strconv.ParseInt(timestamp, 10, 64)
+	if err != nil {
+		log.Printf("failed strconv.ParseInt%v\n", err)
+		return false
+	}
+
+	tSince := time.Since(time.Unix(ts, 0))
+	diff := time.Duration(abs64(int64(tSince)))
+	if diff > 5*time.Minute {
+		log.Println("timed out")
+		return false
+	}
+
+	// Not timeouted, then check Mac
 	return checkMAC(message, slackSignature, slackSigningSecret)
+}
+
+func abs64(n int64) int64 {
+	y := n >> 63
+	return (n ^ y) - y
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module main
 go 1.12
 
 require (
+	cloud.google.com/go v0.41.0 // indirect
 	github.com/nicewook/slack-translate/api v0.1.0
 	github.com/nlopes/slack v0.5.0
 )
